c040e5c124e375c3b44369eb963b40ec5d0048e2
RandomY-2 <69881587+RandomY-2@users.noreply.github.com>
2024-02-17 21:13:08 -0800

    project setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Init Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
