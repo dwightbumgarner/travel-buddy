eb249635dba74bde05f683dd57df60642721d79a
yvonneding <dyiwen@umich.edu>
2024-03-09 01:10:03 -0800

    add authentication

--- backend/routes/nearby.js
+++ backend/routes/nearby.js
@@ -2,9 +2,10 @@ const express = require('express');
 const {
     getNearbyPOIList
 } = require('../controllers/nearby');
+const { verifyAuthentication } = require('../middlewares/auth');
 
 const nearbyRouter = express.Router();
 
-nearbyRouter.post('/nearby', getNearbyPOIList);
+nearbyRouter.post('/nearby', verifyAuthentication, getNearbyPOIList);
 
 module.exports = nearbyRouter;
